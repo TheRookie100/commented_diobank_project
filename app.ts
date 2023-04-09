303ab90f8c1e9abc255174fc1e6543c0811652a0
TheRookie100 <121271205+TheRookie100@users.noreply.github.com>
2023-04-09 00:18:23 -0300

    Criar um novo tipo de conta a partir da DioAccount

--- app.ts
+++ app.ts
@@ -78,47 +78,43 @@ console.log (peopleAccount.getName()) // Aqui estamos chamando o novo valor que 
 //
 
 
-
-
-
-/*
-
-// Pai
-abstract class Apps { // DioAccount.ts
-  private name: string
-  private readonly accountNumber: number // privado somente leitura
-  balance: number = 0
-  private status: boolean = true
-
-  constructor(name: string, accountNumber: number){
-    this.name = name
-    this.accountNumber = accountNumber
-  }
-//          arrow function | parametro      
-  public setName = (name: string): void => { // = "setName = (name: string): void => { }"
-    this.name = name
-    console.log('Nome alterado com sucesso!')
-  }
-}
-
-//Filho
-class PeopleAccount extends Apps { // PeopleAccount.ts
-  doc_id: number // exemplo: cpf
-
-  constructor(doc_id: number, name: string, accountNumber: number){
-    super(name, accountNumber)
-    this.doc_id = doc_id
-  }
-}
-
 //Objeto
-const peopleAccount: PeopleAccount = new PeopleAccount (1, "Pedro Lucas" , 10)
-console.log (peopleAccount)
-peopleAccount.setName("Guilherme Rezende") // atribuido um novo valor no objeto usando set na classe pai abstrata
-console.log (peopleAccount)
+const peopleAccount10: PeopleAccount = new PeopleAccount (1, "Pedro Lucas" , 10)
+console.log (peopleAccount10)
+peopleAccount10.setName("Guilherme Rezende") // atribuido um novo valor no objeto usando set na classe pai abstrata
+console.log (peopleAccount10)
+/*
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
+peopleAccount.deposit_(100)
+console.log(peopleAccount)
+
+const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+companyAccount.deposit_(3600)
+console.log(companyAccount)
+
+//                                                id   nome   n▫ conta
+const account1: PeopleAccount = new PeopleAccount(1 , "Joe", 1000)
+console.log(account1) //  toda informacao da conta com seus atributos e metodos
+account1.deposit_(9000) // valor depositado
+account1.withdraw_(150) // valor sacado
+// console.log(account1.balance) // saldo atual
 
 
+const account2: PeopleAccount = new PeopleAccount(23 , "Luccas" , 639)
+console.log(account2) //  toda informacao da conta com seus atributos e metodos
+account2.deposit_(40000) // valor depositado
+account2.withdraw_(2500) // valor sacado
+// console.log(account2.balance) // saldo atual
 
+const CompanyAccount100: CompanyAccount = new CompanyAccount("Alex" , 1)
+console.log(CompanyAccount100)
+CompanyAccount100.deposit_(70000)
+CompanyAccount100.withdraw_(3600)
+CompanyAccount100.getLoan(100000)
+
+const EmployeeAccount22: EmployeeAccount = new EmployeeAccount(10 , 'alex' , 100)
+EmployeeAccount22.deposit_(100)
+console.log(EmployeeAccount22)
 */
 
 
